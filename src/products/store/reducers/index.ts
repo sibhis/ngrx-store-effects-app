f271fb943025f554af9c16390eb6d116a5416f83
SIBHI S <sibhi.s@fayausa.com>
2019-04-12 16:57:51 +0530

    Implement router state and entity composition

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,4 +1,4 @@
-import { ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store';
+import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
 
 import * as fromPizzas from './pizzas.reducer';
 
@@ -22,19 +22,4 @@ export const getProductState = createFeatureSelector<ProductsState>('products');
 //   }
 // }
 
-//pizza state
-export const getPizzaState = createSelector(
-  getProductState,
-  (state: ProductsState)=>state.pizzas
-);
 
-export const getPizzasEntities = createSelector(getPizzaState, fromPizzas.getPizzasEntities);
-export const getAllPizzas = createSelector(
-  getPizzasEntities,
-  (entities) => {
-    return Object.keys(entities)
-    .map(id => entities[parseInt(id,10)])
-  }
-)
-export const getPizzasLoaded = createSelector(getPizzaState, fromPizzas.getPizzasLoaded);
-export const getPizzasLoading = createSelector(getPizzaState, fromPizzas.getPizzasLoading);

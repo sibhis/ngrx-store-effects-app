5a30a1fd17c39ab2dd937c0a2bd4d6dc90932fe8
SIBHI S <sibhi.s@fayausa.com>
2019-04-16 13:03:07 +0530

    Implement effects for toppings to get toppings from services

---
+++ src/products/store/effects/toppings.effect.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@angular/core";
+
+import { Actions, Effect } from '@ngrx/effects';
+import { map, switchMap, catchError } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
+
+import * as toppingsActions from '../actions/toppings.action';
+import * as fromServices from '../../services/toppings.service';
+@Injectable()
+export class ToppingsEffects {
+  constructor(
+    private actions$: Actions,
+    private toppingsService: fromServices.ToppingsService
+  ) {}
+
+@Effect()
+loadToppings$ = this.actions$.ofType(toppingsActions.LOAD_TOPPINGS).pipe(
+  switchMap(
+    () => {
+      return this.toppingsService.getToppings()
+      .pipe(
+        map( toppings => new toppingsActions.LoadToppingsSuccess(toppings)),
+        catchError(error => of(new toppingsActions.LoadToppingsFail(error)))
+      );
+
+    })
+  );
+
+}
+

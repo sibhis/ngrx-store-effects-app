36e2efb803556bfa957467be6b7373cca7108fab
SIBHI S <sibhi.s@fayausa.com>
2019-04-24 18:37:05 +0530

    Activate guard and pre load the products when in another page

--- src/products/containers/products/products.component.ts
+++ src/products/containers/products/products.component.ts
@@ -40,7 +40,7 @@ export class ProductsComponent implements OnInit {
     //   state => { this.pizzas = state }
     // );
     this.pizzas$ = this.store.select(fromStore.getAllPizzas);
-    this.store.dispatch(new fromStore.LoadPizzas());
+    //this.store.dispatch(new fromStore.LoadPizzas());
     this.store.dispatch(new fromStore.LoadToppings());
   }
 }

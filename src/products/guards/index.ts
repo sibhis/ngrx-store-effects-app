51377f10f6f7cdde14b964c6f90dd233c0cbf4da
SIBHI S <sibhi.s@fayausa.com>
2019-04-30 11:57:00 +0530

    Implement guard for products and toppings

--- src/products/guards/index.ts
+++ src/products/guards/index.ts
@@ -1,5 +1,9 @@
 import { PizzasGuard } from './pizzas.guard';
+import { ToppingsGuard } from './toppings.guard';
+import { PizzaExistsGuards } from './pizza-exists.guard';
 
-export const guards: any[] = [PizzasGuard];
+export const guards: any[] = [PizzasGuard, PizzaExistsGuards, ToppingsGuard];
 
 export * from './pizzas.guard';
+export * from './toppings.guard';
+export * from './pizza-exists.guard';
